83d6fb7fe7f48db06647df6073f28377d9be7d16
Joseph <joseph.vavala@epfl.ch>
2018-05-20 16:49:57 +0200

    bridge initial set up

--- QuestVR/Assets/HolyShift/Scripts/CheckForStandardAssets.cs
+++ QuestVR/Assets/HolyShift/Scripts/CheckForStandardAssets.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
 using UnityEditor;
 
 [ExecuteInEditMode]
@@ -17,3 +18,4 @@ public class CheckForStandardAssets : MonoBehaviour {
 		
 	}
 }
+#endif
